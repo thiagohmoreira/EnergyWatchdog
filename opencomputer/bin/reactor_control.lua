6b3dfac8d1a2222a4934bbe6888e96f4a48d84b8
Thiago Hummel Moreira <contato@thiagomoreira.info>
2016-04-19 00:33:56 -0300

    Improving reactor control script.
    - Changing output to be more a GUI then a log file (maybe add the logging to file option in future).
    - Increased main loop frequency to 1Hz (from 0.2Hz).
    - Added more information on capacitor charge/discharge.
    - Improving comments on file.

--- opencomputer/bin/reactor_control.lua
+++ opencomputer/bin/reactor_control.lua
@@ -2,37 +2,74 @@ package.path = '/EnergyWatchdog/lib/?.lua;/EnergyWatchdog/conf/?.lua;' .. packag
 
 local component = require('component')
 local shell = require("shell")
-local logger = require("logger")
 local util = require("util")
+local term = require("term")
+local computer = require("computer")
 
 local function main(args, options)
-    local minEnergy = 0.5 -- %
-    local maxEnergy = 0.95 -- % (we want to shutdown before 100% to reduce waste, turbines should continue producing energy for a while)
-    local sleepTime = 5  -- secs
+    local minChargePercentage = 0.5 -- %
+    local maxChargePercentage = 0.95 -- % (we want to shutdown before 100% to reduce waste, turbines should continue producing energy for a while)
+    local sleepTime = 1  -- secs
 
     -- Init components / data
     local reactor = component.getPrimary("br_reactor")
     local capacitor = component.getPrimary("tile_blockcapacitorbank_name")
     local maxEnergyStored = capacitor.getMaxEnergyStored() -- RF
 
+    -- Clear the terminal screen
+    term.clear()
+
+    local lastUpdate
+    local lastEnergyStored = capacitor.getEnergyStored()
     while true do
-        local currentEnergyStored = capacitor.getEnergyStored() / maxEnergyStored -- %
+        -- Gather needed data
+        local now = computer.uptime()
+        local currentEnergyStored = capacitor.getEnergyStored()
 
-        logger.debugf(
-          "Max Capacity: %s RF - Current Charge: %s RF (%.2f %%)",
-          util.commaInt(maxEnergyStored),
-          util.commaInt(capacitor.getEnergyStored()),
-          currentEnergyStored * 100
-        )
+        -- Calculate the charge I/O
+        local deltaCharge = (currentEnergyStored - lastEnergyStored) / sleepTime -- RF/sec
+        local emptyFullETA = math.abs(math.floor(currentEnergyStored / deltaCharge)) -- sec => Time to empty/fill capacitor
 
-        if (currentEnergyStored <= minEnergy and not reactor.getActive()) then
-            logger.log(logger.INFO, "Turning ON the reactor")
+        -- Calculate the charge percentage and ativate or deactivate reactor accordingly
+        local chargePercentage = currentEnergyStored / maxEnergyStored -- %
+        if (chargePercentage <= minChargePercentage and not reactor.getActive()) then
             reactor.setActive(true)
-        elseif (currentEnergyStored >= maxEnergy and reactor.getActive()) then
-            logger.log(logger.INFO, "Turning OFF the reactor")
+            lastUpdate = now
+        elseif (chargePercentage >= maxChargePercentage and reactor.getActive()) then
             reactor.setActive(false)
+            lastUpdate = now
         end
 
+        -- Write output
+        -- TODO: Check if not cleanning screen, only writing to it won't be an issue
+        term.setCursor(1, 1)
+        term.write(string.format(
+                "Current Time: %s\n\n" ..
+
+                "Capacitor Monitoring\n" ..
+                "  Charge: %s / %s RF (%.2f %%)\n" ..
+                "  I/O: %s RF/sec | ETA: %s\n\n" ..
+
+                "Reactor Monitoring\n" ..
+                "  State: %s | Last update: %s",
+
+            util.commaInt(now),
+
+            util.commaInt(capacitor.getEnergyStored()),
+            util.commaInt(maxEnergyStored),
+            chargePercentage * 100,
+
+            util.commaFloat(deltaCharge, 2),
+            util.secsToTime(emptyFullETA),
+
+            reactor.getActive() and 'Active' or 'Inactive',
+            lastUpdate == nil and '-' or util.commaInt(lastUpdate)
+        ))
+
+        -- Update the last energy read
+        lastEnergyStored = currentEnergyStored
+
+        -- Sleep for a while ^^
         os.sleep(sleepTime)
     end
 end
