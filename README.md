56c1eadb41b774b4a24f2f0f78a3a60fa09f1789
Thiago Hummel Moreira <contato@thiagomoreira.info>
2016-05-12 23:56:03 -0300

    Improving EnderIO capacitor bank support with Computronics
    - Refatored the reactor_control.lua script to monitor a capacitor bank (the hole multiblock structure) instead of multiple 'isolated' capacitors.
    - Improved display of uptime with a more human friendly format.
    - Updated the README file to reflect these changes and added a list of dependencies of this script.

--- README.md
+++ README.md
@@ -2,12 +2,12 @@
 
 This is an [OpenComputers](https://oc.cil.li) / [PHP](https://secure.php.net/)
 implementation of a controller for reactor and turbines from
-[Big Reactors](http://www.big-reactors.com/) with capacitors from
+[Big Reactors](http://www.big-reactors.com/) with capacitors banks from
 [EnderIO](http://enderio.com/).
 
-## What it will do?
+## What it is?
 
-The main idea is to make an script that will monitor a capacitor bank, a reactor
+The main idea is to make a script that will monitor a capacitor bank, a reactor
 and some turbines, to be as efficient as possible in terms of fuel consumption,
 energy generation and availability.
 
@@ -18,14 +18,26 @@ plant even when not logged to the game.
 ## What it already does?
 
 Only really basic functions are ready at the moment. The most useful one is
-reactor_control.lua that will check one capacitor and when it reaches 50%
+reactor_control.lua that will check one capacitor bank and when it reaches 50%
 charge, it will turn on the reactor. Once the capacitor reaches 95%, it will
 shut down the reactor. There is no control on reactor configs, no checks at all,
 really simple for now.
 
-Also on the PHP side nothing is really functional, there is just a script I used
+Also on the PHP side nothing is really functional, there is just a script used
 to test connectivity.
 
 ## Installation
+
+#### Dependencies
+
+- [Minecraft](https://minecraft.net/en/) v1.7.10
+- [Forge](http://files.minecraftforge.net/maven/net/minecraftforge/forge/index_1.7.10.html)
+- [OpenComputers](https://oc.cil.li)
+- [Computronics](http://wiki.vex.tty.sh/wiki:computronics)
+  - [AsieLib](http://wiki.vex.tty.sh/wiki:asielib)
+- [Big Reactors](http://www.big-reactors.com/)
+- [EnderIO](http://enderio.com/)
+
+#### Script Setup
 
 TODO
--- opencomputer/bin/reactor_control.lua
+++ opencomputer/bin/reactor_control.lua
@@ -7,8 +7,6 @@ local computer = require("computer")
 
 local util = require("util")
 local prettyTable = require("prettyTable")
-local Capacitor = require("Capacitor")
-
 
 local function main(args, options)
     local minChargePercentage = 0.5 -- %
@@ -17,12 +15,7 @@ local function main(args, options)
 
     -- Init components / data
     local reactor = component.getPrimary("br_reactor")
-
-    -- TODO: Ensure same capacitor order across restarts
-    local capacitors = {}
-    for addr in component.list("tile_blockcapacitorbank_name") do
-      table.insert(capacitors, Capacitor:new(addr, computer.uptime()))
-    end
+    local capacitor = component.getPrimary("capacitor_bank")
 
     -- TODO: Ensure same turbine order across restarts
     local turbines = {}
@@ -65,37 +58,48 @@ local function main(args, options)
             head = {
                 '#',
                 'Charge (RF | %)',
-                'I/O (RF/sec)',
+                'I/O (RF/t)',
                 'ETA'
             },
-            colWidths = { 1, 32, 12, -50 },
+            colWidths = { 1, 45, 12, -50 },
             style = { marginLeft = 2, compact = true }
         }
         local minCharge
 
-        local chargePercentage = 1
-        for i, capacitor in ipairs(capacitors) do
-            capacitor:update(now)
-            capacitorsInfo:push({
-                i,
-                util.commaInt(capacitor.proxy.getEnergyStored()) .. ' / ' ..
-                util.commaInt(capacitor.maxEnergyStored) .. ' (' ..
-                util.commaFloat(capacitor.chargePercentage * 100, 2) .. ')',
-                util.commaFloat(capacitor.avgChargeIO, 2),
-                util.secsToTime(capacitor.emptyFullETA)
-            })
+        local energyStored = capacitor.getEnergyStored()
+        local maxEnergyStored = capacitor.getMaxEnergyStored()
+        local energyStoredPer = (energyStored / maxEnergyStored)
+        local averageChangePerTick = capacitor.getAverageChangePerTick()
 
-            -- Get the emptier capacitor
-            if capacitor.chargePercentage < chargePercentage then
-                chargePercentage = capacitor.chargePercentage
+        local emptyFullETA
+        if averageChangePerTick == 0 then
+            emptyFullETA = 0
+        else
+            local energy
+            if averageChangePerTick > 0 then
+                energy = maxEnergyStored - energyStored
+            else
+                energy = energyStored
             end
+
+            emptyFullETA = math.floor(energy / (averageChangePerTick * 20))
         end
 
+        capacitorsInfo:push({
+            1,
+            util.commaInt(energyStored) .. ' / ' ..
+            util.commaInt(maxEnergyStored) .. ' (' ..
+            util.commaFloat(energyStoredPer * 100, 2) .. ')',
+            util.commaFloat(averageChangePerTick, 2),
+            util.secsToTime(emptyFullETA)
+        })
+
+
         -- Check for reactor activation need
-        if chargePercentage <= minChargePercentage and not reactor.getActive() then
+        if energyStoredPer <= minChargePercentage and not reactor.getActive() then
             reactor.setActive(true)
             lastUpdate = now
-        elseif chargePercentage >= maxChargePercentage and reactor.getActive() then
+        elseif energyStoredPer >= maxChargePercentage and reactor.getActive() then
             reactor.setActive(false)
             lastUpdate = now
         end
@@ -139,10 +143,10 @@ local function main(args, options)
                 "Turbine Monitoring\n\n" ..
                 "%s\n",
 
-            util.commaInt(now),
+            util.secsToTime(now) .. string.rep(string.char(32), 50),
 
             reactor.getActive() and 'Active' or 'Inactive',
-            lastUpdate == nil and '-' or util.commaInt(lastUpdate) .. string.rep(string.char(32), 50),
+            lastUpdate == nil and '-' or util.secsToTime(lastUpdate) .. string.rep(string.char(32), 50),
             reactorRodsInfo:toString(),
             capacitorsInfo:toString(),
             turbinesInfo:toString()
